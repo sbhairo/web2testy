137126549189101186e7b2525fbd3af0e7f0bf31
sbhairo <31744151+sbhairo@users.noreply.github.com>
2017-09-07 17:24:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ console.log('hello Shyan!')
 document.write('<h1> Hi!</h1>')
 
 adj_list = ["exciting", "boring", "fucked up"]
-prompt("This summer I went on a very " + adj_list[Math.floor(adj_list.length*Math.random())] +  " adventure")
+write("This summer I went on a very " + adj_list[Math.floor(adj_list.length*Math.random())] +  " adventure")
