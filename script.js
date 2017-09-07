59085077b1052a02a89af5df68bc71a077cdc1c1
sbhairo <31744151+sbhairo@users.noreply.github.com>
2017-09-07 17:10:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log('hello Shyan!')
 document.write('<h1> Hi!</h1>')
+
 adj_list = ["exciting", "boring", "fucked up"]
 write("This summer I went on a very " + adj_list[Math.floor(adj_list.length*Math.random())] +  " adventure")
